27af2b249e42054bdacb2e7e252d94d8eeb23a0a
Shashwat Yashaswi <shashwatyashaswi@gmail.com>
2016-10-15 00:04:57 +0530

    Adding student.h which declares the class students. More elements maybe added to the class as they cross my mind.

---
+++ student.h
@@ -0,0 +1,33 @@
+/*
+	*********************Things to include in documentation***************************
+	*
+	*************************************END******************************************
+*/
+
+/*
+	********************************Possible Errors***********************************
+	*
+	***********************************************************************************
+*/
+
+#ifndef __STUDENTS__
+#define __STUDENTS__
+
+#define <cstring>
+
+struct students{
+	int rollNo;			//Stores roll no.
+	string name;		//Stores name
+	double marks;		//Stores marks
+	students operator= ();
+	//Overloading << operator to use in cout
+	friend ostream& operator<< (ostream &, const students);
+	//To check equality of two operator.
+	bool operator== (const students) const;
+	//Some constructors including one copy constructor
+	students ();
+	students (const students &);
+	students (int, string, double);
+}
+
+#endif
